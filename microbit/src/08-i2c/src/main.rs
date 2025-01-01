c23131e4c41a5d2f5698a95bbc6d637888ca51d0
Avraham Appel <aappel248@gmail.com>
2025-01-01 11:55:59 -0500

    Read microbit v1.3 accelerator using mma8x5x driver

--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -3,50 +3,32 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
 
-use microbit::hal::prelude::*;
+//use microbit::hal::prelude::*;
+use mma8x5x::Mma8x5x;
 
 #[cfg(feature = "v1")]
-use microbit::{
-    hal::twi,
-    pac::twi0::frequency::FREQUENCY_A,
-};
-
-#[cfg(feature = "v2")]
-use microbit::{
-    hal::twim,
-    pac::twim0::frequency::FREQUENCY_A,
-};
-
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
-
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+use microbit::{hal::twi, pac::twi0::frequency::FREQUENCY_A};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
+    let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
 
-    #[cfg(feature = "v1")]
-    let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
-
-    #[cfg(feature = "v2")]
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
-
-    let mut acc = [0];
-    let mut mag = [0];
-
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
-
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
-
-    loop {}
+    let sensor = Mma8x5x::new_mma8653(i2c);
+    let mut sensor = sensor.into_active().ok().unwrap();
+    loop {
+        match sensor.read() {
+            Ok(data) => {
+                rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+            }
+            Err(e) => {
+                rprintln!("Error: {:?}", e);
+            }
+        }
+    }
 }
